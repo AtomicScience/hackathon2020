1ce2bcab486b89b65c7066d69e62441d225ac623
ilau020203 <antip19052016@gmail.com>
2020-10-17 12:34:50 +0300

    test1

--- site.js
+++ site.js
@@ -1,15 +1,50 @@
 // Перечисление зависимостей:
 var path = require('path');
-var express = require('express');
-
+var express = require('express');//install by npm
+const mongoose = require('mongoose')//install by npm
+const exphbs = require('express-handlebars')//install by npm
+const todoRoutes = require('./routes/admin')
 // Описание настроек:
+/*
 var staticSiteOptions = {
    portnum: 80, // слушать порт 80
    maxAge: 1000 * 60 * 15 // хранить страницы в кэше пятнадцать минут
-};
+};*/
 
 // Запуск сайта:
-express().use(express.static(
-   path.join(__dirname, 'static'),
-   staticSiteOptions
-)).listen(staticSiteOptions.portnum);
+const PORT = process.env.PORT || 3000
+const app = express()
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
+
+async function start() {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://ilau020203:ilau020203@cluster0.dn8qc.mongodb.net/<dbname>?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false
+      }
+    )
+    app.listen(PORT, () => {
+      console.log('number port:',PORT)
+      
+      console.log('Server has been started...')
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
