e28d032eb5719a8d1ec534997b753a2f998af70f
AtomicScience <dmitry.greblyuk@mail.ru>
2020-10-17 12:45:34 +0300

    Leaflet package added

--- site.js
+++ site.js
@@ -1,15 +1,18 @@
 // Перечисление зависимостей:
 var path = require('path');
 var express = require('express');
+var config = require("./config.json");
 
 // Описание настроек:
 var staticSiteOptions = {
-   portnum: 80, // слушать порт 80
-   maxAge: 1000 * 60 * 15 // хранить страницы в кэше пятнадцать минут
+   portnum: config.portnum, // слушать порт 80
+   maxAge: config.maxAge // хранить страницы в кэше пятнадцать минут
 };
 
 // Запуск сайта:
 express().use(express.static(
    path.join(__dirname, 'static'),
    staticSiteOptions
-)).listen(staticSiteOptions.portnum);
+)).listen(config.portnum, function() {
+   console.log(`Server listening at port ${config.portnum}`);
+});
